e299724b7c1cf17d1c3dcb2475e2bb83d17fa8fe
ehnandwa <nandwaharry24@gmail.com>
2021-07-16 02:25:51 +0300

    filter

--- src/components/GameFilter/index.tsx
+++ src/components/GameFilter/index.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, ReactElement} from "react";
 import {GENRES, PLATFORMS, SORT_BY, TAGS} from "components/GameFilter/constants";
+import {Form, Label, Select} from "components/GameFilter/styles";
 
 
 interface Props {
@@ -7,52 +8,52 @@ interface Props {
 }
 const GameFilter = ({ onChange }: Props):ReactElement => (
 
-    <form onChange={onChange}>
-        <label htmlFor="platform-select">
+    <Form onChange={onChange}>
+        <Label htmlFor="platform-select">
             Platform:
-            <select name="platform" id="platform-select">
+            <Select name="platform" id="platform-select">
 
                 {PLATFORMS.map(platform => (
                     <option key={platform.value} value={platform.value}>{platform.display}</option>
                 ))}
 
-            </select>
+            </Select>
 
-        </label>
-        <label htmlFor="genre-select">
+        </Label>
+        <Label htmlFor="genre-select">
             Genre:
-            <select name="genre" id="genre-select">
+            <Select name="genre" id="genre-select">
 
                 {GENRES.map(genre => (
                     <option key={genre.value} value={genre.value}>{genre.display}</option>
                 ))}
 
-            </select>
+            </Select>
 
-        </label>
-        <label htmlFor="tag-select">
+        </Label>
+        <Label htmlFor="tag-select">
             Tag:
-            <select name="tag" id="tag-select">
+            <Select name="tag" id="tag-select">
 
                 {TAGS
                     .map(tag => (
                     <option key={tag.value} value={tag.value}>{tag.display}</option>
                 ))}
 
-            </select>
+            </Select>
 
-        </label>
-        <label htmlFor="sortBy-select">
+        </Label>
+        <Label htmlFor="sortBy-select">
             SORT BY:
-            <select name="sortBy" id="sortBy-select">
+            <Select name="sortBy" id="sortBy-select">
 
                 {SORT_BY.map(sortBy => (
                     <option key={sortBy.value} value={sortBy.value}>{sortBy.display}</option>
                 ))}
 
-            </select>
+            </Select>
 
-        </label>
-    </form>
+        </Label>
+    </Form>
 )
 export default GameFilter
